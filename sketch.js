3da5ca2cc725c707c562633b79d507d1b5f83c5f
Santiago Blasco Arnaiz <santymosby@gmail.com>
2020-10-22 13:43:28 +0200

    Comentarios

--- sketch.js
+++ sketch.js
@@ -1,17 +1,16 @@
 class Sketch{
 	constructor(){
-
+		//Crea un canvas y lo modela según las necesidades
 		let sketchCanvas = document.createElement('canvas');
 		sketchCanvas.id = "sketchCanvas";
 		sketchCanvas.style.position = "relative";
 		sketchCanvas.style.zIndex = 0;
+		//Div en el que se ubica el canvas
 		let gameDiv = document.getElementById("gameDiv");
 		gameDiv.appendChild(sketchCanvas);
 
 		gameDiv.style.width = sketchWidth * scale + "px";
 		gameDiv.style.height = sketchHeight * scale + "px";
-
-
 
 
 		this.sketch = document.getElementById("sketchCanvas");
@@ -27,20 +26,23 @@ class Sketch{
 		this.ctx.fillStyle = sketchColor;
 		this.ctx.fillRect(0, 0, sketchWidth * scale, sketchHeight * scale);
 
+		//Inicializa la manzana con su primera ubicación
 		this.apple = {x:1, y:1};
 	}
-	
+	//Función para dibujar un cuadrado del tamaño de la escala
+	//y color y posición indicados
 	drawSquare(x, y, color){
 
 		this.ctx.fillStyle = color;
 		this.ctx.fillRect(x * scale, y * scale, scale, scale);
 	}
-
+	//Dibuja todo el talero de juego 
 	drawSketch(){
-
+		//Primero el fondo
 		this.ctx.fillStyle = sketchColor;
 		this.ctx.fillRect(0, 0, sketchWidth * scale, sketchHeight * scale);
 
+		//Después la serpiente
 		for(let i = 0; i < snake.tail.length; i++){
 			if(i%2 == 0){
 				this.drawSquare(snake.tail[i].x, snake.tail[i].y, currentColor1);
@@ -50,9 +52,10 @@ class Sketch{
 			
 		}
 
+		//Y por último la manzana
 		this.drawSquare(this.apple.x, this.apple.y, appleColor);
 	}
-
+	//Cambia la posición de la manzana a una nueva aleatoria y libre
 	newApple(snake){
 
 		let empty = false;
