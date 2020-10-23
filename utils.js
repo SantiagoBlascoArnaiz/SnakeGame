6ea9e1abb4add04630bd91767ea1b864cfbb47a2
Santiago Blasco Arnaiz <santymosby@gmail.com>
2020-10-23 11:10:51 +0200

    Colores

--- utils.js
+++ utils.js
@@ -4,28 +4,30 @@ var sketchWidth = sketchHeight = 30;
 
 var sketchColor = "#535353";
 
-var snakeColor = "#F7F7F7";
-
-var green1 = "#AED581"; //Verde
+var green1 = "#AED581"; //Verde 0
 var green2 = "#7CB342";
 
-var turqoise1 = "#80CBC4";  //Turquesa
+var turqoise1 = "#80CBC4";  //Turquesa 1
 var turqoise2 = "#4DB6AC";
 
-var orange1 = "#FFB300";  //Naranja
+var orange1 = "#FFB300";  //Naranja 2
 var orange2 = "#FFA000";
 
-var blue1 = "#90CAF9";	//Azul
+var blue1 = "#90CAF9";	//Azul 3
 var blue2 = "#64B5F6";
 
-var purple1 = "#CE93D8";	//Morado
+var purple1 = "#CE93D8";	//Morado 4
 var purple2 = "#BA68C8";
 
-var pink1 = "#F8BBD0";	//Rosa
+var pink1 = "#F8BBD0";	//Rosa 5
 var pink2 = "#F48FB1";
+
+var snakeColors = [green1, green2, turqoise1, turqoise2, orange1, orange2, blue1, blue2, purple1, purple2, pink1, pink2];
 
 var currentColor1 = green1;
 var currentColor2 = green2;
+
+var currentColor = 0;
 
 
 var appleColor = "#E51267";
